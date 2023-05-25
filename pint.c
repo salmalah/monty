60eb2f33d28548c1aa73813356af894f76266724
salmalah <s.salmalahmidi@gmail.com>
2023-05-25 10:23:15 -0700

    test

--- pint.c
+++ pint.c
@@ -10,7 +10,7 @@ void pint_line(stack_t **stack, unsigned int n)
 {
 	if (*stack == NULL)
 	{
-		fprintf(stderr, "L%u: can't pint, stack empty\n", n);
+		fprintf(stderr, "L%d: can't pint, stack empty\n", n);
 		exit(EXIT_FAILURE);
 	}
 	printf("%d\n", (*stack)->n);
