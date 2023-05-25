55a0a424bee39226293a9b4aad2e0e52fd416d59
salmalah <s.salmalahmidi@gmail.com>
2023-05-25 11:29:00 -0700

    test

--- sub.c
+++ sub.c
@@ -8,13 +8,18 @@
  */
 void sub_l(stack_t **stack, unsigned int num)
 {
+	stack_t = *s;
+	int sub;
+
+	s = *stack;
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
 		fprintf(stderr, "L%d: can't sub, stack too short\n", num);
 		exit(EXIT_FAILURE);
 	}
 
-	(*stack)->next->n -= (*stack)->n;
+	s = s->next->n - s->n;
 	pop_element(stack, num);
+	s->n = s;
 }
 
