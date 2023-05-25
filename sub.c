5b2f7858ebee7333d6ae50e2bc65546ebb231b70
salmalah <s.salmalahmidi@gmail.com>
2023-05-25 11:25:01 -0700

    test

--- sub.c
+++ sub.c
@@ -1,19 +1,20 @@
 #include "monty.h"
 /**
- * sub_l - A function that subtracts the top element of the stack from the second top element
+ * sub_l - A function that subtracts the
+ * top element of the stack from the second top element
  * @stack: The head
  * @num: Line number
  * Return: Nothing
  */
 void sub_l(stack_t **stack, unsigned int num)
 {
-    if (*stack == NULL || (*stack)->next == NULL)
-    {
-        fprintf(stderr, "L%d: can't sub, stack too short\n", num);
-        exit(EXIT_FAILURE);
-    }
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't sub, stack too short\n", num);
+		exit(EXIT_FAILURE);
+	}
 
-    (*stack)->next->n -= (*stack)->n;
-    pop_element(stack, num);
+	(*stack)->next->n -= (*stack)->n;
+	pop_element(stack, num);
 }
 
