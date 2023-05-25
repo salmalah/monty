e2eccbc612ab28b3191c3c000a67c5287e17e3ae
salmalah <s.salmalahmidi@gmail.com>
2023-05-25 10:01:58 -0700

    test

--- Functions2.c
+++ Functions2.c
@@ -33,7 +33,6 @@ void swap_element(stack_t **stack, unsigned int n)
 	{
 		fprintf(stderr, "L%d: can't swap, stack too short\n", n);
 		exit(EXIT_FAILURE);
-		free(stack);
 	}
 	s->next = next->next;
 	if (s->next != NULL)
